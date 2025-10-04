ab4f06c4293f501b90f04c57e4c9ec36b8f92fb3
Mellagui <amine.mellagui2000@gmail.com>
2025-10-04 18:36:00 +0100

    add empty files

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,4 +1,8 @@
 pub mod cp;
 pub mod echo;
+pub mod rm;
+pub mod cd;
+pub mod pwd;
+pub mod mkdir;
 
 pub use echo::echo;
---
+++ src/cmd/rm.rs
@@ -0,0 +1,5 @@
+use std::fs;
+/// cp utility according to https://pubs.opengroup.org/onlinepubs/9699919799/utilities/cp.html
+pub fn pwd(args: &[String]) -> i32 {
+
+}

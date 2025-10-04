9bb374040ae66410343a48d31f1ee009dbc02934
Mellagui <amine.mellagui2000@gmail.com>
2025-10-04 19:48:59 +0100

    add should be do, and what should be handle

--- src/cli/run_command.rs
+++ src/cli/run_command.rs
@@ -6,6 +6,9 @@ pub fn run_command(cmd: &str, args: &[String]) -> Result<String, String> {
     match cmd {
         "echo" => echo(args),
         "cp" => cp::cp(args),
+        "cd" => cd::cd(args),
+        "rm" => rm::rm(args),
+        "pwd" => pwd::pwd(args),
         "mkdir" => mkdir::mkdir(args),
         "exit" => process::exit(0),
         _ => Err(format!("Command '{}' not found", cmd)),

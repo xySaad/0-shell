619d9bdd49499fdfa159e7a95f9a8a60900d955e
uma-oo <oumayma.elfahsi99@gmail.com>
2025-10-12 21:58:59 +0100

    save changes

--- src/cmd/ls.rs
+++ src/cmd/ls.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{ Path, PathBuf };
 use std::io;
 use std::fs::{ self, DirEntry, Metadata, Permissions };
 use std::os::unix::fs::{ FileTypeExt, MetadataExt, PermissionsExt };
@@ -71,13 +71,19 @@ pub fn ls(args: &Vec<String>) -> i32 {
     ls_config.parse();
 
     for target_path in read_target_path(&ls_config) {
+        //println!(" target path :{:?}", target_path);
         match target_path {
-            Ok((target_name, entries_vec)) => {
+            Ok(entries_vec) => {
                 for ent in &entries_vec {
+                    //println!("entry {:?}", ent);
                     let new_entry = Entry::new(ent);
                     match new_entry {
-                        Ok(valid_entry) => {
-                            eprintln!("{}",valid_entry.long_format());
+                        Ok(mut valid_entry) => {
+                            if ls_config.l_falg_set {
+                                eprintln!("{}", valid_entry.long_format());
+                            } else {
+                                eprintln!("{}", valid_entry.regular_format());
+                            }
                         }
                         Err(e) => {
                             eprintln!("{:?}", e);
@@ -100,31 +106,48 @@ pub fn ls(args: &Vec<String>) -> i32 {
 // to be fixed later
 pub fn read_target_path(
     ls_config: &LsConfig
-) -> impl Iterator<Item = Result<(String, Vec<DirEntry>), io::Error>> {
+) -> impl Iterator<Item = Result<Vec<PathBuf>, io::Error>> {
     ls_config.target_paths.iter().map(|target_path| {
-        match fs::read_dir(target_path) {
-            Ok(entries) => {
-                let dir_entries: Result<Vec<DirEntry>, io::Error> = entries.collect();
+        let path = Path::new(target_path);
+        if path.is_dir() {
+            match fs::read_dir(target_path) {
+                Ok(entries) => {
+                    let mut paths = Vec::new();
+                    for entry in entries {
+                        match entry {
+                            Ok(valid_entry) => {
+                                paths.push(valid_entry.path());
+                            }
+                            Err(err) => {
+                                eprintln!("{}", err);
+                            }
+                        }
+                    }
 
-                let vect_entries = match dir_entries {
-                    Ok(vec) => if ls_config.a_flag_set {
-                        vec
+                    let paths = if ls_config.a_flag_set {
+                        paths.push(Path::new(".").to_path_buf());
+                        paths.push(Path::new("..").to_path_buf());
+                        paths.sort_by(|a, b| a.file_name().cmp(&b.file_name()));
+                        paths
                     } else {
-                        vec.into_iter()
-                            .filter(|entry| !entry.file_name().to_string_lossy().starts_with("."))
-                            .collect()
-                    }
-                    Err(e) => {
-                        return Err(e);
-                    }
-                };
+                        paths
+                            .clone()
+                            .into_iter()
+                            .filter(
+                                |entry|
+                                    !entry.file_name().unwrap().to_string_lossy().starts_with(".")
+                            )
+                            .collect::<Vec<_>>()
+                    };
 
-                return Ok((target_path.clone(), vect_entries));
+                    return Ok(paths.clone());
+                }
+                Err(e) => {
+                    return Err(e);
+                }
             }
-            Err(e) => {
-                return Err(e);
-            }
-        };
+        }
+        Ok(vec![Path::new(target_path).to_path_buf()])
     })
 }
 
@@ -137,7 +160,7 @@ enum FileTypeEnum {
     BlockDevice,
     Socket,
     NamedPipe,
-    Unknown,
+    // Unknown,
 }
 
 #[derive(Debug)]
@@ -151,60 +174,85 @@ pub struct Entry {
     entry_name: String,
     file_type: FileTypeEnum,
     is_executable: RefCell<bool>,
+    colored_string: RefCell<ColoredString>,
 }
 
 impl Entry {
-    pub fn new(dir: &DirEntry) -> Result<Self, io::Error> {
-        let mut metadata = match dir.metadata() {
+    pub fn new(dir: &PathBuf) -> Result<Self, io::Error> {
+        let metadata = match dir.metadata() {
             Ok(some_metadata) => some_metadata,
             Err(e) => {
                 return Err(e);
             }
         };
+
         Ok(Self {
             permissions: metadata.permissions(),
             file_type: Self::get_file_type(&metadata),
             size: metadata.size(),
             number_of_links: metadata.nlink(),
             last_modified: metadata.mtime(),
-            entry_name: dir.file_name().to_string_lossy().to_string(),
+            entry_name: Self::get_entry_name(dir),
             onwer_name: Self::get_user_name(metadata.uid()),
             group_name: Self::get_group_name(metadata.gid()),
             is_executable: RefCell::new(false),
+            colored_string: RefCell::new(Self::get_entry_name(dir).white()),
         })
+    }
+
+    fn get_entry_name(dir: &PathBuf) -> String {
+        if dir == Path::new(".") {
+            ".".to_string()
+        } else if dir == Path::new("..") {
+            "..".to_string()
+        } else {
+            dir.file_name()
+                .map(|name| name.to_string_lossy().to_string())
+                .unwrap_or_else(|| dir.to_string_lossy().to_string())
+        }
     }
 
     fn color_entry_name(&self) {
         let entry_type = &self.file_type;
         let is_executable = self.is_executable.borrow();
-        let color: Color = match true {
-            _ if *entry_type == FileTypeEnum::Directory => Color::Blue,
+        println!("entry_type: {:?}", entry_type);
+        match true {
+            _ if *entry_type == FileTypeEnum::Directory => {
+                *self.colored_string.borrow_mut() = self.entry_name.clone().blue().bold();
+            },
             _ if
                 *entry_type == FileTypeEnum::BlockDevice ||
                 *entry_type == FileTypeEnum::CharDevice ||
                 *entry_type == FileTypeEnum::NamedPipe
-            => Color::Yellow,
-            _ if *entry_type == FileTypeEnum::Symlink => Color::Cyan,
-           
-            _ if *entry_type ==  FileTypeEnum::Socket => Color::White,
-            _ if *entry_type == FileTypeEnum::Regular && *is_executable == true => Color::Green,
-            // _ if entry_type == FileTypeEnum::Regular && is_image(self.enyr) == true => Color::Green,
-            // _ if entry_type == FileTypeEnum::Regular && self.is_executable.borrow() == true => Color::Green,
-            _  => Color::BrightBlack,
-            
+            => {
+                *self.colored_string.borrow_mut() = self.entry_name.clone().bold().yellow();
+            },
+            _ if *entry_type == FileTypeEnum::Symlink => {
+                println!("cyan!!");
+                *self.colored_string.borrow_mut() = self.entry_name.cyan();
+            }
+
+            _ if *entry_type == FileTypeEnum::Socket => {
+                *self.colored_string.borrow_mut() = self.entry_name.clone().bold().white();
+            }
+            _ if *entry_type == FileTypeEnum::Directory && *is_executable == true => {
+                *self.colored_string.borrow_mut() = self.entry_name.clone().bold().green();
+            }
+            _ => {
+                *self.colored_string.borrow_mut() = self.entry_name.clone().bold().white();
+            }
         };
     }
 
     fn get_file_type(metadata: &Metadata) -> FileTypeEnum {
         match true {
-            _ if metadata.file_type().is_file() => FileTypeEnum::Regular,
             _ if metadata.file_type().is_dir() => FileTypeEnum::Directory,
             _ if metadata.file_type().is_symlink() => FileTypeEnum::Symlink,
             _ if metadata.file_type().is_block_device() => FileTypeEnum::BlockDevice,
             _ if metadata.file_type().is_char_device() => FileTypeEnum::CharDevice,
             _ if metadata.file_type().is_fifo() => FileTypeEnum::NamedPipe,
             _ if metadata.file_type().is_socket() => FileTypeEnum::Socket,
-            _ => FileTypeEnum::Unknown,
+            _ => FileTypeEnum::Regular,
         }
     }
     fn get_user_name(u_id: u32) -> String {
@@ -238,7 +286,7 @@ impl Entry {
             FileTypeEnum::NamedPipe => "p",
             FileTypeEnum::Socket => "s",
             FileTypeEnum::Regular => "-",
-            FileTypeEnum::Unknown => "?",
+            // FileTypeEnum::Unknown => "?",
         };
 
         let mode = self.permissions.mode();
@@ -281,25 +329,24 @@ impl Entry {
     // "%s %u %s %s %u %s %s\n", <file mode>, <number of links>,
     //  <owner name>, <group name>, <number of bytes in the file>,
     //  <date and time>, <pathname>
-    pub fn long_format(&self) -> String {
+    pub fn long_format(&mut self) -> String {
+        self.color_entry_name();
         format!(
-            "{} {} {} {} {:>5} {} {}",
+            "{} {:>5} {} {} {:>5} {} {}",
             self.format_file_mode(),
             self.number_of_links,
             self.onwer_name,
             self.group_name,
             self.size,
             self.format_date(),
-            self.entry_name
+            *self.colored_string.borrow()
         )
     }
     // they need to be aligned ;)
     pub fn regular_format(&self) -> String {
-        self.entry_name.clone()
+        self.color_entry_name();
+        format!("{}", *self.colored_string.borrow())
     }
 }
 
 // pub fn append_file_type_indicator() {}
-
-
-

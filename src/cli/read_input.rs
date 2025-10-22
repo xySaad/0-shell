3e30e90f46c35330c2a629800c03603ab99f9f04
Mellagui <amine.mellagui2000@gmail.com>
2025-10-22 16:52:02 +0100

    save changes

--- src/cli/read_input.rs
+++ src/cli/read_input.rs
@@ -31,7 +31,7 @@ pub fn read_input() {
             continue;
         }
 
-        let _exit_status = run_command(command);
+        let exit_status = run_command(command);
         let _ = cli::print("$ ");
     }
 }
--- src/cli/run_command.rs
+++ src/cli/run_command.rs
@@ -1,6 +1,6 @@
 use std::process::exit;
 
-use libc::{STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO, c_int, close, fork, waitpid};
+use libc::{STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO, c_int, close, dup2, fork, waitpid};
 
 // pub fn run_command(cmd: &str, args: &[String]) -> Result<String, String> {
 //     match cmd {

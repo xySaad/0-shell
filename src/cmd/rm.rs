ec6c8308064c20c5624d47d05fef5ecc87082103
xySaad <saadrm.dev@gmail.com>
2025-10-27 14:55:33 +0100

    fix: argument parsing

--- src/cmd/rm.rs
+++ src/cmd/rm.rs
@@ -1,7 +1,7 @@
-use std::{env, fs, path::PathBuf};
 use crate::utils::error::clear_error;
+use std::{env, fs, path::PathBuf};
 
-pub fn rm(args: &[String]) -> i32 {
+pub fn rm(mut args: &[String]) -> i32 {
     if args.is_empty() {
         eprintln!("rm: missing operand");
         return 1;
@@ -9,53 +9,68 @@ pub fn rm(args: &[String]) -> i32 {
     println!("{:?}", args);
 
     let mut recursive = false;
-    let mut paths: Vec<&String> = Vec::new();
+    let mut paths: Vec<String> = Vec::new();
     let end_opt = args.iter().position(|val| val == &("--").to_string());
 
-    for (i, val) in args.iter().enumerate() {
-        match end_opt {
-            Some(index) => {
-                match index != i {
-                    true =>  {
-                        match &(*val.to_string()) {
-                            "-r" => recursive = true,
-                            _ => {
-                                if index < i {
-                                    if val != "-" && val.starts_with("-") {
-                                        eprintln!("rm: unrecognized option '{}'", val);
-                                        return 1;
-                                    }
-                                    else { paths.push(val); }
-                                }
-                                else { paths.push(val); }
-                            }
-                        };
-                    },
-                    _ => ()
-                }
-            },
-            None => {
-                match &(*val.to_string()) {
-                    "-r" => recursive = true,
-                    _ => {
-                        if val != "-" && val.starts_with("-") {
-                            eprintln!("rm: unrecognized option '{}'", val);
-                            return 1;
-                        }
-
-                        else { paths.push(val); }
-                    }
-                };
-            }
-        };
+    if let Some(idx) = end_opt {
+        paths = args[idx + 1..].to_vec();
+        args = &args[..idx];
     }
+
+    for opperand in args {
+        match opperand.as_str() {
+            "-r" => recursive = true,
+            option if option.starts_with("-") && option.len() > 1 => {
+                eprintln!("rm: unrecognized option '{}'", option);
+            }
+            _ => paths.push(opperand.to_string()),
+        }
+    }
+
+    // for (i, val) in args.iter().enumerate() {
+    //     match end_opt {
+    //         Some(index) => match index != i {
+    //             true => {
+    //                 match &(*val.to_string()) {
+    //                     "-r" => recursive = true,
+    //                     _ => {
+    //                         if index < i {
+    //                             if val != "-" && val.starts_with("-") {
+    //                                 eprintln!("rm: unrecognized option '{}'", val);
+    //                                 return 1;
+    //                             } else {
+    //                                 paths.push(val);
+    //                             }
+    //                         } else {
+    //                             paths.push(val);
+    //                         }
+    //                     }
+    //                 };
+    //             }
+    //             _ => (),
+    //         },
+    //         None => {
+    //             match &(*val.to_string()) {
+    //                 "-r" => recursive = true,
+    //                 _ => {
+    //                     if val != "-" && val.starts_with("-") {
+    //                         eprintln!("rm: unrecognized option '{}'", val);
+    //                         return 1;
+    //                     } else {
+    //                         paths.push(val);
+    //                     }
+    //                 }
+    //             };
+    //         }
+    //     };
+    // }
 
     if paths.is_empty() {
         eprintln!("rm: missing operand");
         return 1;
     }
 
-    for arg in paths {
+    for arg in &paths {
         let mut path = env::current_dir().unwrap_or_else(|_| PathBuf::from("."));
         path.push(arg);
 
@@ -79,11 +94,10 @@ pub fn rm(args: &[String]) -> i32 {
                 if let Err(err) = fs::remove_dir_all(&path) {
                     eprintln!("rm: cannot remove '{}': {}", arg, clear_error(err));
                 }
+            } else {
+                eprintln!("rm: cannot remove '{}': Is a directory", arg);
             }
-
-            else { eprintln!("rm: cannot remove '{}': Is a directory", arg); }
         }
-
         // File
         else {
             if let Err(err) = fs::remove_file(&path) {
